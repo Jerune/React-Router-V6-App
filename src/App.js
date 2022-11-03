6b8cd0a764dac0b508b58c50c4ae384cfef175d8
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-03 11:13:23 +0100

    feat: Setting up React Router & Routes
    BrowserRouter is initialised in index.js as well as all Routes have been setup in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Teachers from './components/Teachers'
+import Courses from './components/Courses'
+import Header from './components/Header'
 
 function App () {
   return (
     <div className="container">
-
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='teachers' element={<Teachers />} />
+        <Route path='courses' element={<Courses />} />
+      </Routes>
     </div>
   )
 }
